9d7235514dca7fdcec2d4b9b330c6bc8aecc4688
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2020-10-17 16:09:02 -0300

    Criação de model de imagem e abstração para visualizar os dados no retorno da api

--- src/controllers/orphanagesController.ts
+++ src/controllers/orphanagesController.ts
@@ -1,21 +1,26 @@
-import { request, Request, response, Response } from 'express';
+import { request, Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Opharnage';
+import orphanageView from '../views/orphanageView';
 
 export default {
     async index(req: Request, res: Response) {
         const orphanageRepo = getRepository(Orphanage);
-        const orphanages = await orphanageRepo.find();
+        const orphanages = await orphanageRepo.find({
+            relations: ['images']
+        });
 
-        return res.json(orphanages)
+        return res.json(orphanageView.renderMany(orphanages))
     },
     async show(req: Request, res: Response) {
         const { id } = request.params;
 
         const orphanageRepo =  getRepository(Orphanage);
-        const orphanage = await orphanageRepo.findOneOrFail(id);
+        const orphanage = await orphanageRepo.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return res.json(orphanage);
+        return res.json(orphanageView.render(orphanage));
     },
     async create(req: Request, res: Response) {
         const {
@@ -27,6 +32,11 @@ export default {
             opening_hours,
             open_on_weekends
         } = req.body;
+
+        const reqImages = req.files as Express.Multer.File[];
+        const images = reqImages.map(image => {
+            return { path : image.filename }
+        });
     
         const orphanagesRepo = getRepository(Orphanage);
         const orphanage = orphanagesRepo.create({
@@ -36,7 +46,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         });
     
         await orphanagesRepo.save(orphanage);
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import orphanageController from './controllers/orphanagesController';
-
+import uploadConfig from './config/upload';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages', orphanageController.index);
 routes.get('orphanage/:id', orphanageController.show);
-routes.post('/orphanages', orphanageController.create);
+routes.post('/orphanages', upload.array('images'), orphanageController.create);
 
 export default routes;
